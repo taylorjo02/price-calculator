d9e0496b279ab342416dd221ddfc405833f07ab0
taylorjo02 <joshua.d.taylor122@gmail.com>
2025-03-06 23:54:58 -0500

    feat: add prices package, rename module

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module price-calculator
+module github.com/taylorjo02/price-calculator
 
 go 1.24.0
--- main.go
+++ main.go
@@ -1,20 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/taylorjo02/price-calculator/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		taxIncludedPrices := make([]float64, len(prices))
-		for i, price := range prices {
-			taxIncludedPrices[i] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
-
-	fmt.Println(result)
 }
