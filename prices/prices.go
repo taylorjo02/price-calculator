5103b070dc54f420281e24b7b903c613539a724c
taylorjo02 <joshua.d.taylor122@gmail.com>
2025-03-07 00:13:58 -0500

    feat(prices): add loadPrices function, process function, methods to the Job struct, and ability to read input prices in from file

--- prices/prices.go
+++ prices/prices.go
@@ -1,6 +1,11 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
@@ -8,11 +13,15 @@ type TaxIncludedPriceJob struct {
 	TaxIncludedPrices map[string]float64
 }
 
-func (job TaxIncludedPriceJob) Process() {
-	result := make(map[string]float64)
+func (job *TaxIncludedPriceJob) Process() {
+
+	job.LoadPrices()
+
+	result := make(map[string]string)
 
 	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
 
 	fmt.Println(result)
@@ -24,4 +33,39 @@ func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
 		InputPrices:       []float64{10, 20, 30},
 		TaxIncludedPrices: map[string]float64{},
 	}
+}
+
+func (job *TaxIncludedPriceJob) LoadPrices() {
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println("unable to open file: %w", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("scanner error, %w", err)
+		file.Close()
+	}
+
+	prices := make([]float64, len(lines))
+
+	for i, line := range lines {
+		floatPrice, err := strconv.ParseFloat(line, 64)
+		if err != nil {
+			fmt.Println("error converting string value to float, %w", err)
+			file.Close()
+		}
+
+		prices[i] = floatPrice
+	}
+
+	job.InputPrices = prices
 }
